af4a89dcd41513df494642a7369b5f4fdae122fd
6002524 <jrgaln.m@gmail.com>
2024-01-12 13:22:34 +0100

    Se ha refactorizado y comentado el código de FilterActivity y FragmentPopUp.

--- app/src/main/java/com/example/listafacturaspractica/ui/view/FilterActivity.kt
+++ app/src/main/java/com/example/listafacturaspractica/ui/view/FilterActivity.kt
@@ -9,24 +9,36 @@ import com.example.listafacturaspractica.R
 import com.example.listafacturaspractica.databinding.ActivityFilterBinding
 import com.example.listafacturaspractica.databinding.ActivityMainBinding
 
+/**
+ * Clase que representa la Activity de Filtros.
+ */
 class FilterActivity : AppCompatActivity() {
+
+    /**
+     * Binding para la vista de la Activity.
+     */
     private lateinit var binding: ActivityFilterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFilterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Configura el título de la ToolBar.
         supportActionBar?.setTitle("Filtrar Facturas")
     }
 
+    //Método propio para la creación de un menú.
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_filter_activity, menu)
         return true
     }
 
+    //Método propio para establecer el comportamiento del menú al pulsar sobre él.
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.invoiceMenuFilter -> {
+
+                //Intent que se usa para pasan de FilterActivity a MainActivity.
                 val miIntent = Intent(this, MainActivity::class.java)
                 startActivity(miIntent)
                 true
--- app/src/main/java/com/example/listafacturaspractica/ui/view/FragmentPopUp.kt
+++ app/src/main/java/com/example/listafacturaspractica/ui/view/FragmentPopUp.kt
@@ -8,21 +8,31 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.example.listafacturaspractica.databinding.PopUpBinding
 
+/**
+ * Clase que representa un Fragmento de Diálogo (pop-up) para mostrar información adicional.
+ */
 class FragmentPopUp: DialogFragment() {
+
+    /**
+     * Binding para la vista del Fragmento de Diálogo.
+     */
     private var _binding: PopUpBinding? = null
     private val binding get() = _binding!!
 
+    //Método propio para la creación de un diálogo.
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         _binding = PopUpBinding.inflate(LayoutInflater.from(context))
 
+        //Crea un constructor de diálogo utilizando el contexto de la actividad.
         val builder = AlertDialog.Builder(requireActivity())
         builder.setView(binding.root)
 
-        // Configura el comportamiento de los elementos de diseño
+        //Configura el comportamiento de los elementos de diseño.
         binding.botonCerrarPopUp.setOnClickListener {
-            dismiss() // Cierra el pop-up al hacer clic en el botón "Cerrar"
+            dismiss() //Cierra el pop-up al hacer clic en el botón "Cerrar".
         }
 
+        //Crea el diálogo y configura su apariencia.
         val dialog = builder.create()
         val window = dialog.window
         window?.setBackgroundDrawableResource(android.R.color.transparent) // Fondo transparente
@@ -31,6 +41,7 @@ class FragmentPopUp: DialogFragment() {
         return dialog
     }
 
+    //Método propio para la destrucción del diálogo.
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
